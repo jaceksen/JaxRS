a459d1b7ebe2cafe5e7ad9400fe50d7e77edd872
jaceksen <jaceksen@gmail.com>
2017-08-26 15:24:47 +0200

    170826

--- src/main/java/pl/jaceksen/MyResource.java
+++ src/main/java/pl/jaceksen/MyResource.java
@@ -1,17 +1,22 @@
 package pl.jaceksen;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("test")
+@Singleton
 public class MyResource {
+	
+	private int count;
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod() {
-		return "It works!";
+		count = count + 1;
+		return "Count " + count + " razy.";
 	}
 
 }
